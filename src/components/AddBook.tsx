5af8626a4024641b83fb8779d8f100039367e7eb
r0nfy <r0nfymain@gmail.com>
2022-04-10 20:52:58 +0700

    added eslint config and fixed a few following errors

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -1,15 +1,11 @@
 import styles from './AddBook.module.css'
 
-const AddBook = () => {
-    return (
-        <>
-        <label htmlFor="add-book" className={styles.container}>
-            <img src="plus-icon.svg" alt="plus icon" />
-            <p>Add New</p>  
-        </label>
-        <input className={styles.hidden} type="file" accept=".pdf, .epub, .mobi" name="Add Book" id="add-book" />
-        </>
-    )
-}
+const AddBook = () => (
+  <label htmlFor="add-book" className={styles.container}>
+    <img src="plus-icon.svg" alt="plus icon" />
+    <p>Add New</p>
+    <input className={styles.hidden} type="file" accept=".pdf, .epub, .mobi" name="Add Book" id="add-book" />
+  </label>
+)
 
 export default AddBook
