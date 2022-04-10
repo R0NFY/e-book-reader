5332768c7fa5aa0dcd4d53d0b0d303707d83e7e4
r0nfy <r0nfymain@gmail.com>
2022-04-10 20:10:59 +0700

    added file picker dialog

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -2,10 +2,13 @@ import styles from './AddBook.module.css'
 
 const AddBook = () => {
     return (
-        <button className={styles.container}>
+        <>
+        <label htmlFor="add-book" className={styles.container}>
             <img src="plus-icon.svg" alt="plus icon" />
             <p>Add New</p>  
-        </button>
+        </label>
+        <input className={styles.hidden} type="file" accept=".pdf, .epub, .mobi" name="Add Book" id="add-book" />
+        </>
     )
 }
 
